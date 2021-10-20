cb0a934a6b6c7e4bf04caa1d33fe021b5f618a33
Fabian Salas <sfabian.orozco@gmail.com>
2021-10-19 21:48:39 -0300

    add java 1.1

--- app/assets/javascripts/script.js
+++ app/assets/javascripts/script.js
@@ -1,5 +1,36 @@
 $('document').ready(function() {
+    $(".collapse").hide();
     $("#alertDismissButton").click(function() {
         $("#alertDismiss").toggle();
+    });
+    
+
+
+    $(".btn-primary").click(function(e) {
+        e.preventDefault();
+        $("#collapseExample").slideToggle('slow');
     })
+
+    $("#headingOne").click(function(e) {
+        e.preventDefault();
+        $("#collapseOne").toggle();
+        $("#collapseTwo").hide();
+        $("#collapseThree").hide();
+    })
+
+    $("#headingTwo").click(function(e) {
+        e.preventDefault();
+        $("#collapseOne").hide();
+        $("#collapseTwo").toggle();
+        $("#collapseThree").hide();
+    })
+    
+    $("#headingThree").click(function(e) {
+        e.preventDefault();
+        $("#collapseOne").hide();
+        $("#collapseTwo").hide();
+        $("#collapseThree").toggle();
+    })
+
+    
 })
